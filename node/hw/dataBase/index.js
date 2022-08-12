1a70bdbabcc214eb048329faa623e31b0e45b53e
Mykola1996 <91569829+Mykola1996@users.noreply.github.com>
2022-08-12 17:44:18 +0300

    hw

---
+++ node/hw/dataBase/index.js
@@ -0,0 +1,10 @@
+module.exports = [
+    { name: "roma", age: 21 },
+    { name: "koma", age: 123 },
+    { name: "doma", age: 21 },
+    { name: "choma", age: 43 },
+    { name: "homa", age: 12 },
+    { name: "drona", age: 14 },
+    { name: "test", age: 53 },
+
+]
