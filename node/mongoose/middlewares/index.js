969f7823855fcabe6e4dd85ccedbb979f4515f72
Mykola1996 <91569829+Mykola1996@users.noreply.github.com>
2023-02-01 20:39:57 +0200

    Delete node directory

--- node/mongoose/middlewares/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    carMdlwr: require('./car.middleware'),
-    commonMdlwr: require('./common.middleware'),
-    userMdlwr: require('./user.middleware'),
-};
--- node/mongoose/services/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    carService: require('./car.service'),
-    userService: require('./user.service'),
-}
